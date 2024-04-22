2579a2aaf66f9d5064be6e0dadf5f32e1a49a2be
BeshoySamyRizk <119978269+BeshoySamyRizk@users.noreply.github.com>
2024-04-22 19:35:17 +0200

    login api change

--- Client/src/Components/Login.jsx
+++ Client/src/Components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
     e.preventDefault();
     
     try {
-      const response = await fetch('https://fridge-craft-server.vercel.app/api/users/login', {
+      const response = await fetch('https://fridge-craft-server.vercel.app//api/users/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
